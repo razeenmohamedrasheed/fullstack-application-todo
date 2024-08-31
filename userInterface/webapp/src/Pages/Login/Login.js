528a18a9525fe65969ac364d179ffa4a294c0a6a
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-08-31 20:30:07 +0530

    login API Integrated

--- userInterface/webapp/src/Pages/Login/Login.js
+++ userInterface/webapp/src/Pages/Login/Login.js
@@ -1,20 +1,70 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './Login.css'
 
 function Login() {
-    const [isVisible, setIsVisible] = useState(false);
+    const [formData, setFormData] = useState({
+        userName:'',
+        password:''
+    });
+
+    const handleChangeData=(e)=>{
+        const {name,value} = e.target
+        setFormData((prevFormData)=>({
+            ...prevFormData,
+            [name]:value
+        }))
+    }
+
+    const handleLogin= async(e)=>{
+        e.preventDefault();
+        try{
+            let payload ={
+                username:formData?.userName,
+                password:formData?.password
+            }
+            const response = await fetch('http://localhost:8000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payload),
+            });
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const result = await response.json();
+            console.log('Success:', result);
+
+        }catch(error){
+            console.log(error)
+        }
+        
+    }
+
+    useEffect(()=>{
+        console.log(formData)
+    },[formData])
 
 
     return (
         <div className="parent">
             <div className="wrapper">
                 <h2>Login</h2>
-                <form action="#">
+                <form onSubmit={handleLogin}>
                     <div className="input-box">
-                        <input type="text" placeholder="Enter your email" required />
+                        <input type="text" 
+                               placeholder="Enter your email" 
+                               value={formData?.userName} 
+                               name="userName"
+                               onChange={handleChangeData}/>
                     </div>
                     <div className="input-box">
-                        <input type="password" placeholder="Create password" required />
+                        <input type="password" 
+                               placeholder="Create password" 
+                               value={formData.password}
+                               name="password"
+                               onChange={handleChangeData} />
                     </div>
                     <div className="input-box button">
                         <input type="Submit" value="Sign In" />
