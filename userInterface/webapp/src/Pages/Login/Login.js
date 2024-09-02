024e9b5314e502d37d7122023170f2bf1ca6d70f
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-09-02 23:06:42 +0530

    Update Login.js

--- userInterface/webapp/src/Pages/Login/Login.js
+++ userInterface/webapp/src/Pages/Login/Login.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import './Login.css'
 
 function Login() {
     const [formData, setFormData] = useState({
